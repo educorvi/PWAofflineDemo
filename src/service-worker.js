fbfb4d508946dc78c5deb0a79d2a304fd1cb8730
Julian <neferin42@gmail.com>
2020-01-27 14:38:18 +0100

    Custom sw

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,5 +1,3 @@
-self.importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
-
 if (workbox) {
     console.log(`Yay! Workbox is loaded 🎉`);
     workbox.routing.registerRoute(
--- vue.config.js
+++ vue.config.js
@@ -11,7 +11,7 @@ module.exports = {
     workboxPluginMode: 'InjectManifest',
     workboxOptions: {
       // swSrc is required in InjectManifest mode.
-      swSrc: 'src/sw.js',
+      swSrc: 'src/service-worker.js',
       // ...other Workbox options...
     }
   }
